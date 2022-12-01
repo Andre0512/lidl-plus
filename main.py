493db9cba38c3b55793f13a85f76fcc940004582
Andre Basche <andre.basche@gmail.com>
2022-12-01 01:40:53 +0100

    Improve class structure

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 import base64
 import re
 from datetime import datetime, timedelta
-from pprint import pprint
 
 import requests
-from getuseragent import UserAgent
-from oic.oic import Client
-from oic.utils.authn.client import CLIENT_AUTHN_METHOD
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.ui import WebDriverWait
-from seleniumwire import webdriver
 
 
 class LidlApi:
     _CLIENT_ID = "LidlPlusNativeClient"
-    _AUTH_SITE = "https://accounts.lidl.com"
+    _AUTH_API = "https://accounts.lidl.com"
+    _TICKET_API = "https://tickets.lidlplus.com/api/v1"
     _APP = "com.lidlplus.app"
     _OS = "iOs"
 
-    def __init__(self, refresh_token=""):
+    def __init__(self, refresh_token="", language="DE"):
         self._login_url = ""
         self._code_verifier = ""
         self._refresh_token = refresh_token
         self._expires = None
         self._token = ""
+        self._language = language
 
-    def _get_login_url(self):
+    @property
+    def refresh_token(self):
+        return self._refresh_token
+
+    @property
+    def token(self):
+        return self._token
+
+    def _register_oauth_client(self):
+        from oic.oic import Client
+        from oic.utils.authn.client import CLIENT_AUTHN_METHOD
         if self._login_url:
             return self._login_url
         client = Client(client_authn_method=CLIENT_AUTHN_METHOD, client_id=self._CLIENT_ID)
-        client.provider_config(self._AUTH_SITE)
+        client.provider_config(self._AUTH_API)
         code_challenge, self._code_verifier = client.add_code_challenge()
         args = {
             "client_id": client.client_id,
@@ -44,6 +48,8 @@ class LidlApi:
         return self._login_url
 
     def _get_browser(self, headless=True):
+        from seleniumwire import webdriver
+        from getuseragent import UserAgent
         user_agent = UserAgent(self._OS.lower()).Random()
         try:
             options = webdriver.ChromeOptions()
@@ -63,8 +69,8 @@ class LidlApi:
             'Authorization': f'Basic {base64.b64encode(f"{self._CLIENT_ID}:secret".encode()).decode()}',
             'Content-Type': 'application/x-www-form-urlencoded',
         }
-        response = requests.post(f"{self._AUTH_SITE}/connect/token", headers=headers, data=payload).json()
-        self._expires = datetime.now() + timedelta(seconds=response["expires_in"])
+        response = requests.post(f"{self._AUTH_API}/connect/token", headers=headers, data=payload).json()
+        self._expires = datetime.utcnow() + timedelta(seconds=response["expires_in"])
         self._token = response["access_token"]
         self._refresh_token = response["refresh_token"]
 
@@ -82,8 +88,11 @@ class LidlApi:
         return self._auth(payload)
 
     def login(self, phone, password, country, language, verify_token_func):
+        from selenium.webdriver.common.by import By
+        from selenium.webdriver.support import expected_conditions
+        from selenium.webdriver.support.ui import WebDriverWait
         browser = self._get_browser()
-        browser.get(f"{self._get_login_url()}&Country={country}&language={language}")
+        browser.get(f"{self._register_oauth_client()}&Country={country}&language={language}")
         wait = WebDriverWait(browser, 10)
         wait.until(expected_conditions.visibility_of_element_located((By.ID, "button_welcome_login"))).click()
         wait.until(expected_conditions.visibility_of_element_located((By.NAME, "EmailOrPhone"))).send_keys(phone)
@@ -100,23 +109,25 @@ class LidlApi:
         self._authorization_code(code)
 
     def _default_headers(self):
+        if (not self._token and self._refresh_token) or datetime.utcnow() >= self._expires:
+            self._renew_token()
         if not self._token:
             raise Exception("You need to login!")
         return {'Authorization': f'Bearer {self._token}',
                 'App-Version': '999.99.9',
                 'Operating-System': self._OS,
                 'App': 'com.lidl.eci.lidl.plus',
-                'Accept-Language': "DE"
+                'Accept-Language': self._language
                 }
 
-    def tickets(self, language="DE"):
-        url = f"https://tickets.lidlplus.com/api/v1/{language}/list"
+    def tickets(self):
+        url = f"{self._TICKET_API}/{self._language}/list"
         ticket = requests.get(f"{url}/1", headers=self._default_headers()).json()
         tickets = ticket['records']
         for i in range(2, int(ticket['totalCount'] / ticket['size'] + 2)):
             tickets += requests.get(f"{url}/{i}", headers=self._default_headers()).json()['records']
         return tickets
 
-    def ticket(self, ticket_id, language="DE"):
-        url = f"https://tickets.lidlplus.com/api/v1/{language}/tickets"
+    def ticket(self, ticket_id):
+        url = f"{self._TICKET_API}/{self._language}/tickets"
         return requests.get(f"{url}/{ticket_id}", headers=self._default_headers()).json()
