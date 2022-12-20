06da7a5db8dd1646a6316f5e6f1752007587e095
Andre Basche <andre.basche@gmail.com>
2022-12-20 20:40:42 +0100

    Bump to v0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="lidl-plus",
-    version="0.1.16",
+    version="0.2.0",
     author="Andre Basche",
     description="Fetch receipts and more from Lidl Plus",
     long_description=long_description,
