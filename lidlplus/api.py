c9d31bc22d262af8652d23ce7820405c15a255ac
Andre Basche <andre.basche@gmail.com>
2022-12-20 18:40:25 +0100

    fix pylint errors

--- lidlplus/api.py
+++ lidlplus/api.py
@@ -1,3 +1,6 @@
+"""
+Lidl Plus api
+"""
 import base64
 import logging
 import re
@@ -7,13 +10,31 @@ import requests
 
 from lidlplus.exceptions import WebBrowserException, LoginError
 
+try:
+    from getuseragent import UserAgent
+    from oic.oic import Client
+    from oic.utils.authn.client import CLIENT_AUTHN_METHOD
+    from selenium.common.exceptions import TimeoutException
+    from selenium.webdriver.chrome.service import Service as ChromeService
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support import expected_conditions
+    from selenium.webdriver.support.ui import WebDriverWait
+    from seleniumwire import webdriver
+    from webdriver_manager.chrome import ChromeDriverManager
+    from webdriver_manager.firefox import GeckoDriverManager
+except ImportError:
+    pass
+
 
 class LidlPlusApi:
+    """Lidl Plus api connector"""
+
     _CLIENT_ID = "LidlPlusNativeClient"
     _AUTH_API = "https://accounts.lidl.com"
     _TICKET_API = "https://tickets.lidlplus.com/api/v1"
     _APP = "com.lidlplus.app"
     _OS = "iOs"
+    _TIMEOUT = 10
 
     def __init__(self, language, country, refresh_token=""):
         self._login_url = ""
@@ -26,15 +47,15 @@ class LidlPlusApi:
 
     @property
     def refresh_token(self):
+        """Lidl Plus api refresh token"""
         return self._refresh_token
 
     @property
     def token(self):
+        """Current token to query api"""
         return self._token
 
     def _register_oauth_client(self):
-        from oic.oic import Client
-        from oic.utils.authn.client import CLIENT_AUTHN_METHOD
 
         if self._login_url:
             return self._login_url
@@ -55,11 +76,6 @@ class LidlPlusApi:
         return self._login_url
 
     def _init_chrome(self, headless=True):
-        from seleniumwire import webdriver
-        from getuseragent import UserAgent
-        from webdriver_manager.chrome import ChromeDriverManager
-        from selenium.webdriver.chrome.service import Service as ChromeService
-
         user_agent = UserAgent(self._OS.lower()).Random()
         logging.getLogger("WDM").setLevel(logging.NOTSET)
         options = webdriver.ChromeOptions()
@@ -71,9 +87,6 @@ class LidlPlusApi:
         )
 
     def _init_firefox(self, headless=True):
-        from seleniumwire import webdriver
-        from getuseragent import UserAgent
-        from webdriver_manager.firefox import GeckoDriverManager
 
         user_agent = UserAgent(self._OS.lower()).Random()
         logging.getLogger("WDM").setLevel(logging.NOTSET)
@@ -92,20 +105,21 @@ class LidlPlusApi:
     def _get_browser(self, headless=True):
         try:
             return self._init_chrome(headless=headless)
-        except Exception:
+        # pylint: disable=broad-except
+        except Exception as exc1:
             try:
                 return self._init_firefox(headless=headless)
-            except Exception:
-                raise WebBrowserException
+            except Exception as exc2:
+                raise WebBrowserException from exc1 and exc2
 
     def _auth(self, payload):
+        default_secret = base64.b64encode(f"{self._CLIENT_ID}:secret".encode()).decode()
         headers = {
-            "Authorization": f'Basic {base64.b64encode(f"{self._CLIENT_ID}:secret".encode()).decode()}',
+            "Authorization": f"Basic {default_secret}",
             "Content-Type": "application/x-www-form-urlencoded",
         }
-        response = requests.post(
-            f"{self._AUTH_API}/connect/token", headers=headers, data=payload
-        ).json()
+        kwargs = {"headers": headers, "data": payload, "timeout": self._TIMEOUT}
+        response = requests.post(f"{self._AUTH_API}/connect/token", **kwargs).json()
         self._expires = datetime.utcnow() + timedelta(seconds=response["expires_in"])
         self._token = response["access_token"]
         self._refresh_token = response["refresh_token"]
@@ -123,51 +137,40 @@ class LidlPlusApi:
         }
         return self._auth(payload)
 
-    def login(
-        self, phone, password, verify_token_func, headless=True, verify_mode="phone"
-    ):
-        from selenium.webdriver.common.by import By
-        from selenium.webdriver.support import expected_conditions
-        from selenium.webdriver.support.ui import WebDriverWait
-        from selenium.common.exceptions import TimeoutException
+    @property
+    def _register_link(self):
+        args = {
+            "Country": self._country,
+            "language": f"{self._language}-{self._country}",
+        }
+        params = "&".join([f"{key}={value}" for key, value in args])
+        return f"{self._register_oauth_client()}&{params}"
 
-        if verify_mode not in ["phone", "email"]:
+    def login(self, phone, password, verify_token_func, **kwargs):
+        """Simulate app auth"""
+        if verify_mode := kwargs.get("verify_mode", "phone") not in ["phone", "email"]:
             raise ValueError('Only "phone" or "email" supported')
-        browser = self._get_browser(headless=headless)
-        browser.get(
-            f"{self._register_oauth_client()}&Country={self._country}&language={self._language}-{self._country}"
-        )
+        browser = self._get_browser(headless=kwargs.get("headless", True))
+        browser.get(self._register_link)
         wait = WebDriverWait(browser, 10)
-        wait.until(
-            expected_conditions.visibility_of_element_located(
-                (By.ID, "button_welcome_login")
-            )
-        ).click()
-        wait.until(
-            expected_conditions.visibility_of_element_located((By.NAME, "EmailOrPhone"))
-        ).send_keys(phone)
+        is_visible = expected_conditions.visibility_of_element_located
+        is_clickable = expected_conditions.element_to_be_clickable
+        wait.until(is_visible((By.ID, "button_welcome_login"))).click()
+        wait.until(is_visible((By.NAME, "EmailOrPhone"))).send_keys(phone)
         browser.find_element(By.ID, "button_btn_submit_email").click()
         browser.find_element(By.ID, "button_btn_submit_email").click()
         try:
-            wait.until(
-                expected_conditions.element_to_be_clickable((By.ID, "field_Password"))
-            ).send_keys(password)
+            wait.until(is_clickable((By.ID, "field_Password"))).send_keys(password)
             browser.find_element(By.ID, "button_submit").click()
-            element = wait.until(
-                expected_conditions.visibility_of_element_located(
-                    (By.CLASS_NAME, verify_mode)
-                )
-            )
-        except TimeoutException:
-            raise LoginError("Wrong credentials")
+            element = wait.until(is_visible((By.CLASS_NAME, verify_mode)))
+        except TimeoutException as exc:
+            raise LoginError("Wrong credentials") from exc
         element.find_element(By.TAG_NAME, "button").click()
         verify_code = verify_token_func()
         browser.find_element(By.NAME, "VerificationCode").send_keys(verify_code)
         browser.find_element(By.CLASS_NAME, "role_next").click()
-        code = re.findall(
-            "code=([0-9A-F]+)",
-            browser.requests[-1].response.headers.get("Location", ""),
-        )[0]
+        last_request = browser.requests[-1].response.headers.get("Location", "")
+        code = re.findall("code=([0-9A-F]+)", last_request)[0]
         self._authorization_code(code)
 
     def _default_headers(self):
@@ -186,17 +189,17 @@ class LidlPlusApi:
         }
 
     def tickets(self):
+        """Get list of all tickets"""
         url = f"{self._TICKET_API}/{self._country}/list"
-        ticket = requests.get(f"{url}/1", headers=self._default_headers()).json()
+        kwargs = {"headers": self._default_headers(), "timeout": self._TIMEOUT}
+        ticket = requests.get(f"{url}/1", **kwargs).json()
         tickets = ticket["records"]
         for i in range(2, int(ticket["totalCount"] / ticket["size"] + 2)):
-            tickets += requests.get(
-                f"{url}/{i}", headers=self._default_headers()
-            ).json()["records"]
+            tickets += requests.get(f"{url}/{i}", **kwargs).json()["records"]
         return tickets
 
     def ticket(self, ticket_id):
+        """Get full data of single ticket by id"""
+        kwargs = {"headers": self._default_headers(), "timeout": self._TIMEOUT}
         url = f"{self._TICKET_API}/{self._country}/tickets"
-        return requests.get(
-            f"{url}/{ticket_id}", headers=self._default_headers()
-        ).json()
+        return requests.get(f"{url}/{ticket_id}", **kwargs).json()
--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+"""
+Setup Lidl Plus api
+"""
+
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
